e12fde49a4e1b6e3e5f7409cbe60bf696c35282e
Jose Luis <sicajancoy@gmail.com>
2017-03-16 14:30:20 -0600

    first commit

---
+++ stackadata-stack-xD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stackadata-stack-xD
+//
+//  Created by lion-mane on 3/16/17.
+//  Copyright © 2017 Lionmane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
